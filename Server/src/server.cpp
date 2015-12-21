aad73aded6f917e9c15f3d8af2dcba5b6e150818
nadavsch <nadav658@gmail.com>
2015-12-21 05:42:28 +0200

    TCP added to the server

--- Server/src/server.cpp
+++ Server/src/server.cpp
@@ -1,12 +1,30 @@
 #include <iostream>
 #include <string>
 #include "UDPServer.h"
+#include "TCPServer.h"
 #include <stdlib.h>
 
 using namespace std;
 
-int main(int argc, char * argv[]) {
-	Recv* server = new UDPServer(atoi(argv[1]));
+int main(int argc, char *argv[]) {
+	if(argc < 3)
+	{
+		return -1;
+	}
+	string type = string(argv[1]);
+	Recv* server;
+	if(type == "TCP")
+	{
+		server = new TCPServer(atoi(argv[2]));
+	}
+	else if(type == "UDP")
+	{
+		server = new UDPServer(atoi(argv[2]));
+	}
+	else
+	{
+		return -1;
+	}
 	while (true)
 	{
 	std::string msg = "";
