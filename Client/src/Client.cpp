ba99f8d71ddbcf9f3cd289d6c0722546508fd446
BenChemouny <waekyben@gmail.com>
2015-12-21 05:40:41 +0200

    Added Good Loops

--- Client/src/Client.cpp
+++ Client/src/Client.cpp
@@ -34,7 +34,7 @@ int main(int argc, char *argv[]) {
 	while(command!="-1")
 	{
 		std::getline(std::cin, command);
-		Conn->sendMSG(command,command.length());
+		Conn->sendMSG(command+"\0",command.length()+1);
 		Conn->getAnswer();
 	}
 	return 1;
