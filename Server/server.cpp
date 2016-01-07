1b0d3c71ea79899a6e689ef29c5292bd0dbd8e43
nadavsch <nadav658@gmail.com>
2016-01-07 22:29:46 +0200

    final comments

--- Server/server.cpp
+++ Server/server.cpp
@@ -9,7 +9,7 @@
 #include "Cinema.h"
 #include <pthread.h>
 /**
- * program name: ex4
+ * program name: ex5
  * the operation: this program is the cinema program using the TCP and UDP protocols
  */
 using namespace std;
@@ -17,28 +17,37 @@ struct S_Data {
 	int sock;
 	Recv* server;
 };
+/**
+ * this function handle one client
+ * @param data: the data about the client
+ */
 void *threadfunc(void* data)
 {
+	//retrive the data
 	S_Data sdata = *((S_Data*)data);
+	//get the client sock
 	int clientSock = sdata.sock;
-	cout<<"thread started "<<clientSock<<endl;
+	//get the data about the server
 	Recv* server = sdata.server;
-
-	cout<<"got singleton"<<endl;
 	while (true)
 	{
-		cout<<"waiting "<<clientSock<<endl;
+		//get the message from the client
 		std::string msg = "";
 		msg = server->RecvMSG(clientSock);
-		if(!strcmp(msg.c_str(),"-1"))
+		//if the client is closed we end the function
+		if(!strcmp(msg.c_str(),""))
+		{
 			break;
+		}
+		//get the most updated cinema instance
 		Cinema* current = Cinema::getInstance();
 		string response = current->start(msg);
+		//send the answer to the client
 		server->sendAnswer(response, response.length(), clientSock);
 	}
 }
 int main(int argc, char *argv[]) {
-	cout<<"start"<<endl;
+	//if we dont have enough args we return -1
 	if(argc < 3)
 	{
 		return -1;
@@ -46,6 +55,7 @@ int main(int argc, char *argv[]) {
 	Cinema* current;
 	string type = string(argv[1]);
 	Recv* server;
+	//check that we are using TCP
 	if(type == "TCP")
 	{
 		server = new TCPServer(atoi(argv[2]));
@@ -54,27 +64,23 @@ int main(int argc, char *argv[]) {
 	{
 		return -1;
 	}
-	cout<<"..."<<endl;
-	//current = Cinema::getInstance();
-	//cout<<"instance"<<endl;
-	//list<pthread_t> threads;
 	int clientSock;
+	//the server listen for clients
 	while(true)
 	{
 		pthread_t thread;
 		S_Data data;
-		cout<<"wait for client"<<endl;
+		//wait for a client to connect
 		clientSock = server->WaitForClient(atoi(argv[2]));
-		cout<<"got client"<<endl;
+		//if a client is trying to connect we open a thread that will handle
+		//him
 		data.sock = clientSock;
 		data.server = server;
 		int status = pthread_create(&thread, NULL, threadfunc, (void*)&data);
-		cout<<"open thread "<<status<<endl;
+		//if there was an error we print a message
 		if(status)
 		{
-			//error("error with he thread");
+			cout<<"error!!!"<<endl;
 		}
-		//threads.push_back(thread);
 	}
-
 }
